b74dab0ed23254aae65df75bc52db4461b5dd167
Nguyen_Duc_Tung_Lam <lam.nguyen@rikai.technology>
2024-12-04 15:42:46 +0700

    added C code to be more audience-friendly

---
+++ TheCrazyFrog(c)/TheCrazyFrog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int getTotalWaysToJump(int targetedLeaf)
+{
+	if(targetedLeaf == 1)
+	{
+		return 1;
+	}
+	else if (targetedLeaf == 2)
+	{
+		return 2;
+	}
+	
+	return getTotalWaysToJump(targetedLeaf - 1) + getTotalWaysToJump(targetedLeaf - 2);
+}
+
+int main()
+{
+	int targetedLeaf;
+	printf("\nThe leaf which the frog wants to be is leaf  ");
+	scanf("%d", &targetedLeaf);
+	
+	if(targetedLeaf <= 0)
+	{
+		printf("\nWrong input, the target leaf obviously can't be less than 1, please try again  (:\n");
+		main();
+	}
+	
+	// int totalWaysToJump = getTotalWaysToJump(targetedLeaf);
+	// printf("\nThere're total  %d ways  to jump from leaf 0 to leaf targetedLeaf  :)\n", totalWaysToJump);
+	
+	printf("\nThere're total  %d ways  to jump from leaf 0 to leaf %d  :)\n", getTotalWaysToJump(targetedLeaf), targetedLeaf);
+	
+	// prevents the CLI from closing too soon
+	printf("\nPlease enter an integer to end the program  :))\n");
+	scanf("%*d");
+	return 0;
+}
